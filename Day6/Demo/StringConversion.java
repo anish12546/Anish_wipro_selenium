d9315d83b30cf31d3afdbb7f2f2702cfeb9fa121
anish12546 <jadhavanish415@gmail.com>
2025-08-10 22:14:47 +0530

    Update StringConversion.java

--- Day6/Demo/StringConversion.java
+++ Day6/Demo/StringConversion.java
@@ -4,24 +4,24 @@ public class StringConversion {
     public static void main(String[] args) {
         int originalInt = 123;
 
-        // Convert int to String using String.valueOf(...)
         String stringValue = String.valueOf(originalInt);
         System.out.println("String value: " + stringValue);
 
         try {
-            // Convert String back to int using Integer.parseInt(...)
+           
             int parsedInt = Integer.parseInt(stringValue);
             System.out.println("Parsed int: " + parsedInt);
         } catch (NumberFormatException e) {
-            System.out.println("Error: Invalid format for integer parsing.");
+            System.out.println("invalid");
         }
 
-        // Try with an invalid string to demonstrate exception
-        String invalidString = "123abc";
+        
+        String dString = "123abc";
         try {
-            int badInt = Integer.parseInt(invalidString); // Will throw exception
+            int Int = Integer.parseInt(invalidString);
         } catch (NumberFormatException e) {
-            System.out.println("Caught NumberFormatException for input: \"" + invalidString + "\"");
+            System.out.println("Caught NumberFormatException for input: \"" + String );
         }
     }
 }
+
