78249a4de28604acb9c29d984843a7b456b37e8e
anish12546 <jadhavanish415@gmail.com>
2025-08-10 22:22:12 +0530

    Update TemperatureConvert.java

--- Day6/Demo/TemperatureConvert.java
+++ Day6/Demo/TemperatureConvert.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class TemperatureConvert {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         
         System.out.print("Enter temperature in Celsius: ");
-        double celsius = scanner.nextDouble();
+        double celsius = sc.nextDouble();
 
 
         double fahrenheit = celsius * 9 / 5 + 32;
@@ -23,5 +23,6 @@ public class TemperatureConvert {
        
     }
 }
+
 
 
